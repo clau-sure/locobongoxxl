86c4f21455f240734ec9c69cfe4c2ee4d46e95b9
Claudia Álvarez <claudia.alvarez.arias@gmail.com>
2024-05-15 20:27:41 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import "./assets/Noah Text Heavy.ttf"
+import logo from './assets/logo.svg'
+
 
 function App() {
+  const number = (num) => {
+    return (
+      <div key={num} id={num} className="number">
+        <p>{num}</p>
+      </div>
+    );
+  };
+
+  const numbers = () => {
+    let rows = [];
+    for (let i = 1; i <= 90; i++) {
+      rows.push(number(i));
+    }
+    return rows;
+  };
+
+  const enterAnadir = (event) => {
+    if (event.key === 'Enter') {
+      enterNumber();
+    }
+  }
+
+  const enterNumber = () => {
+    let inputValue = document.getElementById("anadir").value;
+    let classList = document.getElementById(inputValue).classList;
+    if (classList.contains('flip')) {
+      document.getElementById(inputValue).classList.remove('flip');
+    } else {
+      document.getElementById(inputValue).classList.add('flip');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="both">
+      <div className="wrapper">{numbers()}</div>
+      <div className="inputs">
+        <img src={logo} alt="LocoBongo logo" />
+        <div className="inputdiv">
+          <input
+            type="number"
+            id="anadir"
+            min="1"
+            max="90"
+            defaultValue="0"
+            onKeyDown={enterAnadir}
+          ></input>
+        </div>
+      </div>
     </div>
   );
 }
